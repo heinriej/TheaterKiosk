790e43fa4a5c26c0d6f9deca91c9210d205449a0
heinriej <heinirej@mail.uc.edu>
2024-09-30 11:25:06 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        
+        Scanner in = new Scanner(System.in);
+        int age = 0;
+
+        System.out.println("Enter your age: ");
+        age = in.nextInt();
+        if (age >= 21)
+        {
+            System.out.println("Take your paper wristband.");
+        }
+
     }
 }
